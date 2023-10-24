4eb95ccb024f7925fa0590ae0f170ef30cc07dae
manish50kumar <manish5060773@gmail.com>
2023-10-24 16:05:03 +0530

    add some library remove some not working code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-gray-500 text-2xl font-bold text-white'>
+       <h1>Hello World</h1>
+       <h1>Hello World</h1>
+       <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
